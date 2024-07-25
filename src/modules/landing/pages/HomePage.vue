42cc108456d0e059fcd77ae176fb3ad38216b70e
Jenny Zhang <jenny.zhang@procuraduria.gob.pa>
2024-07-25 16:43:53 -0500

    [add]

---
+++ src/modules/landing/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl">Bienvenido</h1>
+
+    <p>Counter: {{ counter }}</p>
+    <button @click="counter++">+1</button>
+  </div>
+</template>
+
+<script lang="ts" src="./Homepage.ts"></script>
---
+++ src/modules/pokemons/pages/PokemonPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h1 class="text-3xl font-bold">
+      Pokemon <small class="text-blue-500">#{{ id }}</small>
+    </h1>
+    <img
+      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`"
+      alt="Pokemon"
+      class="w-[200px] h-[200px] mt-10"
+      width="200"
+      height="200"
+    />
+    <RouterLink
+      class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-10"
+      :to="{ name: 'pokemon', params: { id: id + 1 } }"
+    >
+      Siguiente
+    </RouterLink>
+  </section>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  id: number;
+}
+const props = defineProps<Props>();
+</script>
